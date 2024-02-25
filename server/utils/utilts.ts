acd60cc06afa9c895a7a3418be9eaa63ed4fca5c
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-02-24 23:57:59 -0500

    changed token

--- server/utils/utilts.ts
+++ server/utils/utilts.ts
@@ -2,7 +2,7 @@ import { Role } from "./types";
 import { cohere } from "../controller/controller";
 import { UserInfoType } from "../schemas/schema";
 
-const maxTokens = 200;
+const maxTokens = 150;
 
 export async function queryChat(
   chatHistory: Array<string>,
@@ -10,7 +10,7 @@ export async function queryChat(
   prompt: string,
   userInfo: Array<UserInfoType>
 ) {
-  const startingPrompt = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Maximum response is 500 characters. Please make sure that you're engaging and fun to talk to.
+  const startingPrompt = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Use a maximum of 500 characters. Please make sure that you're engaging and fun to talk to.
 
     The person you're going to talk to is ${userInfo[0].name}. They are currently in ${userInfo[0].status}. Please use these information to make good relevant answers!`;
 
@@ -40,7 +40,7 @@ export async function queryChat(
       appendPrompt += `${userInfo[0].name}: ` + `"${chat}"` + "\n\n";
     }
   }
-  const extendConversation = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Maximum response is 500 characters. Please make sure that you're engaging and fun to talk to.
+  const extendConversation = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Use a maximum of 500 characters. Please make sure that you're engaging and fun to talk to.
 
         The person you're going to talk to is ${userInfo[0].name}. They are currently in ${userInfo[0].status}. Please use these information to make good relevant answers!
         You already had conversation with them or ${userInfo[0].name} is about to ask you something
@@ -63,7 +63,7 @@ export async function queryIntro(
   role: Role,
   userInfo: Array<UserInfoType>
 ) {
-  const startingPrompt = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Maximum response is 500 characters. Please make sure that you're engaging and fun to talk to.
+  const startingPrompt = `You will play the role of a ${role} who's role is to help a user figure out if they want to become a ${role} as their career. Your response format should focus on why someone should become a ${role}. Ask how you can help. Do not break character. Use a maximum of 500 characters. Please make sure that you're engaging and fun to talk to.
 
     The person you're going to talk to is ${userInfo[0].name}. They are currently in ${userInfo[0].status}. Please use these information to make good relevant answers!`;
 
