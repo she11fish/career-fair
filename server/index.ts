75b03150f2750727c1dc1f9294615a7a431d2797
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-02-25 09:13:04 -0500

    merged

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL!);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
