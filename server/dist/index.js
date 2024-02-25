7ec8e6ab6622006b6a0e96eba80c2752b1f113d6
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-02-25 09:10:55 -0500

    fix env issues

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
